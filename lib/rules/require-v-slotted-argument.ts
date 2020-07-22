a90c6b56adad4405b73e98e6311afd58d175da4c
Yosuke Ota <otameshiyo23@gmail.com>
2020-07-22 17:25:14 +0900

    Add `vue-scoped-css/require-v-slotted-argument` rule (#42)

---
+++ lib/rules/require-v-slotted-argument.ts
@@ -0,0 +1,81 @@
+import {
+    getStyleContexts,
+    getCommentDirectivesReporter,
+    StyleContext,
+    ValidStyleContext,
+} from "../styles/context"
+import type { RuleContext, Rule } from "../types"
+import {
+    isPseudoEmptyArguments,
+    isVSlottedPseudo,
+    VSlottedPseudo,
+} from "../styles/utils/selectors"
+
+declare const module: {
+    exports: Rule
+}
+
+module.exports = {
+    meta: {
+        docs: {
+            description:
+                "require selector argument to be passed to `::v-slotted()`.",
+            categories: ["vue3-recommended"],
+            default: "warn",
+            url:
+                "https://future-architect.github.io/eslint-plugin-vue-scoped-css/rules/require-v-slotted-argument.html",
+        },
+        fixable: null,
+        messages: {
+            missingArguments:
+                "Need to pass argument to the `::v-slotted` pseudo-element.",
+        },
+        schema: [],
+        type: "suggestion", // "problem",
+    },
+    create(context: RuleContext) {
+        const styles = getStyleContexts(context)
+            .filter(StyleContext.isValid)
+            .filter((style) => style.scoped)
+        if (!styles.length) {
+            return {}
+        }
+        const reporter = getCommentDirectivesReporter(context)
+
+        /**
+         * Reports the given node
+         * @param {ASTNode} node node to report
+         */
+        function report(node: VSlottedPseudo) {
+            reporter.report({
+                node,
+                loc: node.loc,
+                messageId: "missingArguments",
+            })
+        }
+
+        /**
+         * Verify the style
+         */
+        function verify(style: ValidStyleContext) {
+            style.traverseSelectorNodes({
+                enterNode(node) {
+                    if (
+                        isVSlottedPseudo(node) &&
+                        isPseudoEmptyArguments(node)
+                    ) {
+                        report(node)
+                    }
+                },
+            })
+        }
+
+        return {
+            "Program:exit"() {
+                for (const style of styles) {
+                    verify(style)
+                }
+            },
+        }
+    },
+}
---
+++ tests/lib/rules/require-v-slotted-argument.ts
@@ -0,0 +1,50 @@
+import { RuleTester } from "eslint"
+const rule = require("../../../lib/rules/require-v-slotted-argument")
+
+const tester = new RuleTester({
+    parser: require.resolve("vue-eslint-parser"),
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: "module",
+    },
+})
+
+tester.run("require-v-slotted-argument", rule, {
+    valid: [
+        `
+        <template><div class="item">sample</div></template>
+        <style scoped>
+        .baz .qux ::v-slotted(.foo .bar) {}
+        </style>
+        `,
+    ],
+    invalid: [
+        {
+            code: `
+            <template><div class="item">sample</div></template>
+            <style scoped>
+            .baz .qux ::v-slotted .foo .bar {}
+            .baz .qux ::v-slotted() {}
+            </style>
+            `,
+            errors: [
+                {
+                    message:
+                        "Need to pass argument to the `::v-slotted` pseudo-element.",
+                    line: 4,
+                    column: 23,
+                    endLine: 4,
+                    endColumn: 34,
+                },
+                {
+                    message:
+                        "Need to pass argument to the `::v-slotted` pseudo-element.",
+                    line: 5,
+                    column: 23,
+                    endLine: 5,
+                    endColumn: 36,
+                },
+            ],
+        },
+    ],
+})
